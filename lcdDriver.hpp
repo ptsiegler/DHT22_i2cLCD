d6e08b9bb46e0429978d842a7fbfa5265e77737e
ptsiegler <54459587+ptsiegler@users.noreply.github.com>
2020-03-15 20:57:23 -0400

    Create lcdDriver.hpp

---
+++ lcdDriver.hpp
@@ -0,0 +1,19 @@
+#include "include.hpp"
+
+// Define some device parameters
+#define LCD_WIDTH 20   // Maximum characters per line
+#define LCD_ROWS 4 //The number of rows on the LCD
+
+#define LCD_BEG_LINE_1 0x80 // LCD RAM address for the 1st line
+#define LCD_END_LINE_1 0x93 // LCD RAM address for the 1st line
+
+#define LCD_BEG_LINE_2 0xC0 // LCD RAM address for the 2nd line
+#define LCD_END_LINE_2 0xD3 // LCD RAM address for the 2nd line
+
+#define LCD_BEG_LINE_3 0x94 // LCD RAM address for the 3rd line
+#define LCD_END_LINE_3 0xA7 // LCD RAM address for the 3rd line
+
+#define LCD_BEG_LINE_4 0xD4 // LCD RAM address for the 4th line
+#define LCD_END_LINE_4 0xE7 // LCD RAM address for the 4th line
+
+
