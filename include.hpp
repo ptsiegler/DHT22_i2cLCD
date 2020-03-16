6861f48ac9433e7a1671bc125afe9cea71b58628
ptsiegler <54459587+ptsiegler@users.noreply.github.com>
2020-03-15 20:56:47 -0400

    Create include.hpp
    All the stuff that each file should have

---
+++ include.hpp
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <string.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+#include <wiringPi.h>
+#include <stdint.h>
+#include <time.h>
+#include <iostream>
+
